906859bd4adf17be72f962e240b1f1c1f79185f1
logossoudarryl20 <logossoudarryl20@gmail.com>
2024-08-19 13:35:21 +0000

    initialisation du projet avec creation de la navigation entre pages

---
+++ lib/navigation/my_navigation_bar.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/pages/education_page.dart';
+import 'package:myapp/pages/home_page.dart';
+import 'package:myapp/pages/menu_page.dart';
+import 'package:myapp/pages/message_page.dart';
+import 'package:myapp/pages/profile_page.dart';
+
+class MyNavigationBar extends StatefulWidget {
+  MyNavigationBar({super.key});
+
+  @override
+  State<MyNavigationBar> createState() => _NavigationBarState();
+}
+
+class _NavigationBarState extends State<MyNavigationBar> {
+  int selectedIndex = 0;
+
+  final List<Widget> pages = [
+    HomePage(),
+    EducationPag(),
+    MenuPage(),
+    MessagePage(),
+    ProfilePage(),
+  ];
+
+  void _navigationPages(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[selectedIndex],
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+        child: BottomNavigationBar(
+          onTap: _navigationPages,
+          elevation: 30,
+          backgroundColor: Color.fromARGB(251, 11, 2, 63),
+          type: BottomNavigationBarType.fixed,
+          currentIndex: selectedIndex,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home, color: Colors.white), label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.school, color: Colors.white), label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.apps, color: Colors.white), label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.chat_bubble_rounded, color: Colors.white),
+                label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person, color: Colors.white), label: "Home"),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/education_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class EducationPag extends StatelessWidget {
+  const EducationPag({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("EduPage"),
+      ),
+    );
+  }
+}

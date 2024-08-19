787e0ba525cc36aa96f9c9ae12e068378899d797
logossoudarryl20 <logossoudarryl20@gmail.com>
2024-08-19 14:40:26 +0000

    icons label changed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/models/options_model.dart';
 
 import '../widgets/header_section.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  List<OptionsModel> options = [];
+
+  void _getOptions() {
+    options = OptionsModel.getOptions();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +19,31 @@ class HomePage extends StatelessWidget {
         child: Column(
           children: [
             HeaderSection(),
+            Column(
+              children: [
+                ListView.separated(
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      padding: EdgeInsets.all(8),
+                      height: 30,
+                      decoration: BoxDecoration(
+                        color: Colors.amber,
+                      ),
+                      child: Text(
+                        options[index].text,
+                      ),
+                    );
+                  },
+                  itemCount: options.length,
+                  separatorBuilder: (context, index) =>
+                      const SizedBox(width: 16),
+                ),
+              ],
+            )
           ],
         ),
       ),
     );
   }
 }
-
